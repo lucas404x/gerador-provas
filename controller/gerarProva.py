5996e01ade0ef545ed64688eef53e4f2b9059031
lucas404x <joselucas0303@gmail.com>
2019-07-24 22:15:55 -0300

    diff --git a/controller/gerarProva.py b/controller/gerarProva.py index b953c23..72c2828 100644 --- a/controller/gerarProva.py +++ b/controller/gerarProva.py @@ -1,4 +1,23 @@ +from model.buscarLinks import BuscarLinks +from model.acessarLinks import AcessarLinks +from model.extrairDados import ExtrairDados +from model.escreverProva import EscreverProva
    -def gerar_prova():
    -	pass
    \ No newline at end of file
    +def gerar_prova(materia, assunto):
    +	links = BuscarLinks(materia, assunto)
    +	acessar_links_ = AcessarLinks(links.buscar_links())
    +	dados = ExtrairDados(acessar_links_.acessar_links())
    +
    +	try:
    +		questoes = dados.extrair_dados(0)
    +		respostas = dados.extrair_dados(1)
    +	except Exception as e:
    +		print(e)
    +		return -1
    +	else:
    +		escrever_prova_ = EscreverProva(materia, assunto)
    +		escrever_prova_.escrever_prova(questoes, "questões")
    +		escrever_prova_.escrever_prova(respostas, "respostas")
    +		return 0
    +
    \ No newline at end of file

--- controller/gerarProva.py
+++ controller/gerarProva.py
@@ -1,4 +1,23 @@
+from model.buscarLinks import BuscarLinks
+from model.acessarLinks import AcessarLinks
+from model.extrairDados import ExtrairDados
+from model.escreverProva import EscreverProva
 
 
-def gerar_prova():
-	pass
+def gerar_prova(materia, assunto):
+	links = BuscarLinks(materia, assunto)
+	acessar_links_ = AcessarLinks(links.buscar_links())
+	dados = ExtrairDados(acessar_links_.acessar_links())
+
+	try:
+		questoes = dados.extrair_dados(0)
+		respostas = dados.extrair_dados(1)
+	except Exception as e:
+		print(e)
+		return -1
+	else:
+		escrever_prova_ = EscreverProva(materia, assunto)
+		escrever_prova_.escrever_prova(questoes, "questões")
+		escrever_prova_.escrever_prova(respostas, "respostas")
+		return 0
+	
